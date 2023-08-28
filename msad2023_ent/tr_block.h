8a96e97451f066f1a3ad6f1a41ec4fbbfe9be94c
pocky0517 <okishun0517@gmail.com>
2023-08-28 21:57:21 +0900

    沖オリジナルのリソース

--- msad2023_ent/tr_block.h
+++ msad2023_ent/tr_block.h
@@ -4,7 +4,7 @@
             .composite<BrainTree::MemSequence>() \
                 .composite<BrainTree::ParallelSequence>(1,2) \
                     .leaf<IsColorDetected>(CL_RED) \
-                    .leaf<TraceLine>(20, GS_TARGET, P_CONST, I_CONST, D_CONST, 0.0, TS_NORMAL) \
+                    .leaf<TraceLine>(25, GS_TARGET, P_CONST, I_CONST, D_CONST, 0.0, TS_NORMAL) \
                 .end() \
 /* Reset Arm Pos. \
                 .leaf<SetArmPosition>(ARM_INITIAL_ANGLE, ARM_SHIFT_PWM) \
@@ -22,16 +22,56 @@
             .leaf<IsBackOn>() \
             .composite<BrainTree::MemSequence>() \
                 .composite<BrainTree::ParallelSequence>(1,2) \
-                    .leaf<IsColorDetected>(CL_RED) \
-                    .leaf<TraceLine>(20, GS_TARGET, P_CONST, I_CONST, D_CONST, 0.0, TS_NORMAL) \
+                    .leaf<IsTimeEarned>(3000000) \
+                    .leaf<TraceLine>(25, GS_TARGET, P_CONST, I_CONST, D_CONST, 0.0, TS_NORMAL) \
                 .end() \
 /* Reset Arm Pos. \
                 .leaf<SetArmPosition>(ARM_INITIAL_ANGLE, ARM_SHIFT_PWM) \
 */ \
-                .composite<BrainTree::ParallelSequence>(1,3) \
-                    .leaf<IsSonarOn>(300) \
+                .composite<BrainTree::ParallelSequence>(1,2) \
+                    .leaf<IsTimeEarned>(20500000) \
+                    .leaf<TraceLine>(70, GS_TARGET, P_CONST, I_CONST, D_CONST, 0.0, TS_NORMAL) \
+                .end() \
+                \
+                .composite<BrainTree::ParallelSequence>(1,2) \
+                    .leaf<IsColorDetected>(CL_RED) \
+                    .leaf<TraceLine>(20, GS_TARGET, P_CONST, I_CONST, D_CONST, 0.0, TS_NORMAL) \
+                .end() \
+                \
+                .composite<BrainTree::ParallelSequence>(2,3) \
+                    .leaf<IsSonarOn>(200) \
                     .leaf<IsTimeEarned>(7290000) \
-                    .leaf<RunAsInstructed>(20, 20, 0.5) \
+                    .leaf<RunAsInstructed>(20, 23, 0.5) \
+                .end() \
+                \
+                .composite<BrainTree::ParallelSequence>(1,2) \
+                    .leaf<IsColorDetected>(CL_GREEN) \
+                    .leaf<RunAsInstructed>(-15, -30, 0.0) \
+                .end() \
+                \
+                .composite<BrainTree::ParallelSequence>(1,2) \
+                    .leaf<IsColorDetected>(CL_RED) \
+                    .leaf<RunAsInstructed>(21, 12, 0.0) \
+                .end() \
+                \
+                .composite<BrainTree::ParallelSequence>(1,2) \
+                    .leaf<IsColorDetected>(CL_BLACK) \
+                    .leaf<RunAsInstructed>(25, 25, 0.0) \
+                .end() \
+                \
+                .composite<BrainTree::ParallelSequence>(1,2) \
+                    .leaf<IsColorDetected>(CL_BLUE) \
+                    .leaf<TraceLine>(25, GS_TARGET, P_CONST, I_CONST, D_CONST, 0.0, TS_NORMAL) \
+                .end() \
+                \
+                .composite<BrainTree::ParallelSequence>(1,2) \
+                    .leaf<IsColorDetected>(CL_BLACK) \
+                    .leaf<TraceLine>(30, GS_TARGET, P_CONST, I_CONST, D_CONST, 0.0, TS_OPPOSITE) \
+                .end() \
+                \
+                .composite<BrainTree::ParallelSequence>(1,2) \
+                    .leaf<IsColorDetected>(CL_BLUE) \
+                    .leaf<TraceLine>(50, GS_TARGET, P_CONST, I_CONST, D_CONST, 0.0, TS_OPPOSITE) \
                 .end() \
             .end() \
         .end()
