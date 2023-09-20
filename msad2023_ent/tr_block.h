6dc553623561f5a8acb81faf8691d4821069773f
pocky0517 <okishun0517@gmail.com>
2023-09-20 18:39:24 +0900

    右コース追加

--- msad2023_ent/tr_block.h
+++ msad2023_ent/tr_block.h
@@ -3,9 +3,18 @@
         .composite<BrainTree::ParallelSequence>(1,2) \
             .leaf<IsBackOn>() \
             .composite<BrainTree::MemSequence>() \
-                .composite<BrainTree::ParallelSequence>(1,2) \
+                .composite<BrainTree::ParallelSequence>(1,3) \
                     .leaf<IsColorDetected>(CL_RED) \
+                    .leaf<IsTimeEarned>(12000000) \
                     .leaf<TraceLine>(20, GS_TARGET, P_CONST, I_CONST, D_CONST, 0.0, TS_NORMAL) \
+                .end() \
+                .composite<BrainTree::ParallelSequence>(1,2) \
+                    .leaf<IsTimeEarned>(7000000) \
+                    .leaf<RunAsInstructed>(10, 10, 0.0) \
+                .end() \
+                .composite<BrainTree::ParallelSequence>(1,2) \
+                    .leaf<IsTimeEarned>(1300000) \
+                    .leaf<RunAsInstructed>(-2, -2, 0.0) \
                 .end() \
                 .composite<BrainTree::ParallelSequence>(1,2) \
                     .leaf<IsTimeEarned>(6500000) \
@@ -19,16 +28,18 @@
                     .leaf<IsTimeEarned>(1300000) \
                     .leaf<RotateEV3>(0, 0, 0.0) \
                 .end() \
-                .composite<BrainTree::ParallelSequence>(1,2) \
+                .composite<BrainTree::ParallelSequence>(1,3) \
                     .leaf<IsColorDetected>(CL_RED) \
+                    .leaf<IsTimeEarned>(10000000) \
                     .leaf<RunAsInstructed>(-15, -15, 0.5) \
                 .end() \
                 .composite<BrainTree::ParallelSequence>(1,2) \
                     .leaf<IsTimeEarned>(1300000) \
                     .leaf<RunAsInstructed>(-4, -4, 0.0) \
                 .end() \
-                .composite<BrainTree::ParallelSequence>(1,2) \
+                .composite<BrainTree::ParallelSequence>(1,3) \
                     .leaf<IsColorDetected>(CL_BLACK) \
+                    .leaf<IsTimeEarned>(2300000) \
                     .leaf<RotateEV3>(-180, 20, 0.0) \
                 .end() \
                 .composite<BrainTree::ParallelSequence>(1,2) \
