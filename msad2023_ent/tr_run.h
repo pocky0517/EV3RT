d6dbb023f16a9676aae2ef831635ddea68007f15
pocky0517 <okishun0517@gmail.com>
2023-09-20 18:33:08 +0900

    右コース取り込み

--- msad2023_ent/tr_run.h
+++ msad2023_ent/tr_run.h
@@ -16,20 +16,16 @@
                     .leaf<RunAsInstructed>(85, 80, 0.0) \
                 .end() \
                 .composite<BrainTree::ParallelSequence>(1,2) \
-                    .leaf<IsTimeEarned>(1800000) \
-                    .leaf<RunAsInstructed>(90, 65, 0.0) \
+                    .leaf<IsTimeEarned>(1400000) \
+                    .leaf<RunAsInstructed>(90, 66, 0.0) \
                 .end() \
                 .composite<BrainTree::ParallelSequence>(1,2) \
-                    .leaf<IsTimeEarned>(1500000) \
-                    .leaf<RunAsInstructed>(30, 30, 0.0) \
+                    .leaf<IsColorDetected>(CL_BLUE) \
+                    .leaf<RunAsInstructed>(5, 5, 0.0) \
                 .end() \
                 .composite<BrainTree::ParallelSequence>(1,2) \
-                    .leaf<IsTimeEarned>(3000000) \
-                    .leaf<RunAsInstructed>(10, 10, 0.5) \
-                .end() \
-                .composite<BrainTree::ParallelSequence>(1,2) \
-                    .leaf<IsColorDetected>(CL_BLACK) \
-                    .leaf<RunAsInstructed>(2, 4, 0.5) \
+                    .leaf<IsTimeEarned>(1300000) \
+                    .leaf<RunAsInstructed>(0, 0, 0.0) \
                 .end() \
                 .composite<BrainTree::ParallelSequence>(1,2) \
                     .leaf<IsTimeEarned>(1000000) \
@@ -56,7 +52,7 @@
                     .leaf<RunAsInstructed>(85, 80, 0.0) \
                 .end() \
                 .composite<BrainTree::ParallelSequence>(1,2) \
-                    .leaf<IsTimeEarned>(1650000) \
+                    .leaf<IsTimeEarned>(1400000) \
                     .leaf<RunAsInstructed>(90, 68, 0.0) \
                 .end() \
                 .composite<BrainTree::ParallelSequence>(1,2) \
